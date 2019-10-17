1985ff8185e4dc98f147d02d30809a1e7f70aa75
Geoff Morris <geoffm@gmail.com>
2019-10-16 19:22:29 -0700

    Updates

---
+++ precache-manifest.44392f94e7583a6f56ebbf5a4641782a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2833b2f96b23b4e4d5c4a2df3f7561",
+    "url": "/opower-challenge/index.html"
+  },
+  {
+    "revision": "9b8b455ac63f02921751",
+    "url": "/opower-challenge/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4860d971c7f480e13451",
+    "url": "/opower-challenge/static/js/2.e39b9c47.chunk.js"
+  },
+  {
+    "revision": "9b8b455ac63f02921751",
+    "url": "/opower-challenge/static/js/main.50f6c4d3.chunk.js"
+  },
+  {
+    "revision": "566cb7f8e8f3c9a4d745",
+    "url": "/opower-challenge/static/js/runtime-main.8688b888.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.f0e8b28f01c9d05f4e509a0702743c77.js"
+  "/opower-challenge/precache-manifest.44392f94e7583a6f56ebbf5a4641782a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/opower-challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
