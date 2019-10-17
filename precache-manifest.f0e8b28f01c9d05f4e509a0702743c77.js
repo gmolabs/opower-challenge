bf897f6f4d186e10c259ba9c270d0e636174b778
Geoff Morris <geoffm@gmail.com>
2019-10-16 19:15:39 -0700

    Updates

---
+++ precache-manifest.f0e8b28f01c9d05f4e509a0702743c77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f5ddb047431ebc86cf4d368f089161",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "f39e39146ea527e49ffd",
+    "url": "/myapp/static/css/main.60b98f4a.chunk.css"
+  },
+  {
+    "revision": "2036a66c821f3b6f4eac",
+    "url": "/myapp/static/js/2.da32bb76.chunk.js"
+  },
+  {
+    "revision": "f39e39146ea527e49ffd",
+    "url": "/myapp/static/js/main.971b5c7d.chunk.js"
+  },
+  {
+    "revision": "79a0336c335341982a1c",
+    "url": "/myapp/static/js/runtime-main.b4f3755b.js"
+  }
+]);
