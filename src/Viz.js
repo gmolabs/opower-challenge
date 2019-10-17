1f88ab1800084f9968c3077ce4f6b2ad29686751
Geoff Morris <geoffm@gmail.com>
2019-10-16 21:51:44 -0700

    Fixed svg path

--- src/Viz.js
+++ src/Viz.js
@@ -2,8 +2,13 @@ import React, { useContext } from "react"
 import { VizContext } from "./contexts/VizContext"
 import Paper from "@material-ui/core/Paper"
 import { flexbox } from "@material-ui/system"
+import CarFill from "./img/car-fill.svg"
+import CarOutline from "./img/car-outline.svg"
+import TreeFill from "./img/tree-fill.svg"
+import TreeOutline from "./img/tree-outline.svg"
 
 function Viz({ data, vizMode }) {
+    console.log(CarFill)
     // const { vizMode, setVizMode } = useContext(VizContext)
     const curYear = data.energyUse.currentYear
     const lastYear = data.energyUse.priorYear
@@ -15,8 +20,8 @@ function Viz({ data, vizMode }) {
     console.log(vizMode)
     return (
         <div style={{ padding: 20 }}>
-            {[...Array(Math.abs(n))].map((e, i) => <img src={vizMode == "cars" ? "./img/car-fill.svg" : "./img/tree-fill.svg"} style={{ width: "20%", padding: 10 }} key={i} />)}
-            {n == 0 && <img src={vizMode == "cars" ? "./img/car-outline.svg" : "./img/tree-outline.svg"} style={{ width: "20%", padding: 10 }} />}
+            {[...Array(Math.abs(n))].map((e, i) => <img src={vizMode == "cars" ? CarFill : TreeFill} style={{ width: "20%", padding: 10 }} key={i} />)}
+            {n == 0 && <img src={vizMode == "cars" ? CarOutline : TreeOutline} style={{ width: "20%", padding: 10 }} />}
         </div>
     )
 }
